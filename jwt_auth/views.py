17c1248026539c93f3a9973c2235405670ec24bc
dongra02 <dsgraham1@gmail.com>
2021-01-14 16:02:21 +0200

    RegisterView added

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -8,3 +8,15 @@ from django.conf import settings
 import jwt
 
 from .serializers.common import UserSerializer
+
+
+User = get_user_model()
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        new_user = UserSerializer(data=request.data)
+        if new_user.is_valid():
+            new_user.save()
+            return Response({ 'message': f'Registration Successful, Welcome {new_user.username}' })
+        return Response(new_user.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
