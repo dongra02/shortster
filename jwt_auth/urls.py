d4a615b56aa442b09b2a92937a8ac9f3e2bf7118
dongra02 <dsgraham1@gmail.com>
2021-01-14 16:34:16 +0200

    log in view and url added, ready to test in insomnia

--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LogInView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
+    path('login/', LogInView.as_view()),
 ]
--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from django.contrib.auth import get_user_model
 from django.conf import settings
 import jwt
+from datetime import datetime, timedelta
 
 from .serializers.common import UserSerializer
 
@@ -13,6 +14,7 @@ from .serializers.common import UserSerializer
 User = get_user_model()
 
 class RegisterView(APIView):
+    ''' Requests to /auth/register/ '''
 
     def post(self, request):
         new_user = UserSerializer(data=request.data)
@@ -20,3 +22,30 @@ class RegisterView(APIView):
             new_user.save()
             return Response({ 'message': f'Registration Successful, Welcome {new_user.username}' })
         return Response(new_user.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class LogInView(APIView):
+    ''' Requests to /auth/login/ '''
+
+    def get_user(self, username):
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise PermissionDenied(detail='Invalid Credentials')
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = self.get_user(username)
+
+        if not user.check_password(password):
+            raise PermissionDenied(detail='Invalid Credentials')
+
+        dt = datetime.now() + timedelta(days=7)
+
+        token = jwt.encode(
+            { 'sub': user.id, 'exp': int(dt.strftime('%s')) },
+            settings.SECRET_KEY,
+            algorithm='HS256'
+        )
+
+        return Response({ 'token': token, 'message': f'Welcome Back {user.username}' })
