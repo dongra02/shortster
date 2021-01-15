673614d7445f2dfda4cfbdee8bacfc832c6b9569
dongra02 <dsgraham1@gmail.com>
2021-01-15 09:54:37 +0200

    shorcode.models initial model created

--- shortcode/models.py
+++ shortcode/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Shortcode(models.Model):
+    full_url = models.CharField(max_length=2000)
+    short_url = models.CharField(max_length=6, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_access = models.DateTimeField(null=True, blank=True)
+    access_count = models.IntegerField(default=0)
+
+    def add_access(self):
+        self.access_count += 1
