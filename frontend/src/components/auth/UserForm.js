bae62736177821b14400c38309dd14c447085aed
dongra02 <dsgraham1@gmail.com>
2021-01-17 18:53:40 +0200

    useState working for userform, update styling with useStyles

--- frontend/src/components/auth/UserForm.js
+++ frontend/src/components/auth/UserForm.js
@@ -6,73 +6,78 @@ import { register, login } from '../../lib/api'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
 
-class UserForm extends React.Component {
+const useStyles = makeStyles(() => ({
+  main: {
+    border: '1px solid #43bce7',
+    borderRadius: '5px',
+    maxWidth: '40%',
+    margin: '0 auto',
+    padding: '1rem'
+  }
+}))
 
-  state = {
-    login: true,
-    formData: {
-      username: '',
-      password: '',
-      password_confirmation: ''
-    },
-    formErrors: {}
+
+const UserForm = ({ handleLogin }) => {
+
+  const classes = useStyles()
+  
+  const [loginMode, setMode] = React.useState(true)
+  
+  const initialFormData = { username: '', password: '', password_confirmation: '' }
+  const [formData, setFormData] = React.useState(initialFormData)
+  
+  // const [formErrors, setFormErrors] = React.useState({})
+
+  const changeMode = () =>{
+    setMode(!loginMode)
+    setFormData(initialFormData)
+  }
+
+  const handleChange = (e) => {
+    const newFormData = { ...formData, [e.target.id]: e.target.value }
+    setFormData(newFormData)
   }
   
-  changeMode = () => {
-    const formData = { username: '', password: '', password_confirmation: '' }
-    this.setState({ login: !this.state.login, formData })
-  }
-  
-  handleChange = (e) => {
-    const formData = { ...this.state.formData, [e.target.id]: e.target.value }
-    this.setState({ formData })
-  }
-
-  handleSubmit = async () => {
-    if (!this.state.login) {
+  const handleSubmit = async () => {
+    if (!loginMode) {
       try {
-        const response = await register(this.state.formData)
+        const response = await register(formData)
         console.log(response)
       } catch (err) {
         console.log(err)
       }
     }
     try {
-      const response = await login(this.state.formData)
+      const response = await login(formData)
       localStorage.setItem('token', response.data.token)
-      this.props.handleLogin()
+      handleLogin()
     } catch (err) {
       console.log(err)
     }
   }
 
-  render() {
-
-    const { login } = this.state
-    const { username, password, password_confirmation } = this.state.formData
-
-    return (
-      <Grid container spacing={2} direction='column' justify='center' alignItems='center'>
-        <Grid item>
-          <TextField id='username' label='username' value={username}  onChange={this.handleChange} fullWidth/>
-        </Grid>
-        <Grid item>
-          <TextField id='password' label='password' type='password' value={password} onChange={this.handleChange}/>
-        </Grid>
-        {!login && 
-        <Grid item>
-          <TextField id='password_confirmation' type='password' label='confirm password' value={password_confirmation} onChange={this.handleChange}/>
-        </Grid>}
-        <Grid item>
-          <Button variant='outlined' onClick={this.handleSubmit}>{ login ? 'Login' : 'Register' }</Button>
-        </Grid>
-        <Grid item>
-          <Button variant='outlined' onClick={this.changeMode}>{ login ? 'I\'m a New User' : 'I Have An Account' }</Button>
-        </Grid>
+  return (
+    <Grid className={classes.main} container spacing={2} direction='column' justify='center' alignItems='center'>
+      <Grid item>
+        <TextField id='username' label='username' value={formData.username}  onChange={handleChange} fullWidth/>
       </Grid>
-    )
-  }
+      <Grid item>
+        <TextField id='password' label='password' type='password' value={formData.password} onChange={handleChange}/>
+      </Grid>
+      {!loginMode && 
+      <Grid item>
+        <TextField id='password_confirmation' type='password' label='confirm password' value={formData.password_confirmation} onChange={handleChange}/>
+      </Grid>}
+      <Grid item>
+        <Button variant='outlined' onClick={handleSubmit}>{ loginMode ? 'Login' : 'Register' }</Button>
+      </Grid>
+      <Grid item>
+        <Button variant='outlined' onClick={changeMode}>{ loginMode ? 'I\'m a New User' : 'I Have An Account' }</Button>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default UserForm
