cb1e742074f346a4ff9f7d5be90b609ebe09448c
dongra02 <dsgraham1@gmail.com>
2021-01-19 17:59:45 +0200

    Styling - shortcode list in table form, shaded

--- frontend/src/components/code/CodeList.js
+++ frontend/src/components/code/CodeList.js
@@ -8,21 +8,41 @@ import TableHead from '@material-ui/core/TableHead'
 import TableBody from '@material-ui/core/TableBody'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  main: {
+    maxWidth: '90%',
+    maxHeight: '600px',
+    overflow: 'scroll',
+    outline: '1px solid black'
+  }
+}))
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.primary.main,
+    height: 60
+  }
+}))(TableRow)
 
 const CodeList = ({ userCodes }) => {
+
+  const classes = useStyles()
 
   while (!userCodes) return <div>loading</div>
 
   return (
     <>
-      <TableContainer>
-        <Table>
+      <TableContainer className={classes.main}>
+        <Table size='small'>
           <TableHead>
-            <TableRow>
+            <StyledTableRow>
               <TableCell>Shortcode</TableCell>
               <TableCell>Full URL</TableCell>
               <TableCell>Created</TableCell>
-            </TableRow>
+              <TableCell align='center'>Manage</TableCell>
+            </StyledTableRow>
           </TableHead>
           <TableBody>
             {userCodes.map(code => <CodeListItem key={code.id} {...code} />)}
--- frontend/src/components/code/CodeListItem.js
+++ frontend/src/components/code/CodeListItem.js
@@ -5,12 +5,25 @@ import React from 'react'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined'
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined'
+import { withStyles } from '@material-ui/core/styles'
+
+const StyledTableRow = withStyles(() => ({
+  root: {
+    height: 40,
+    '&:nth-of-type(odd)': {
+      backgroundColor: '#f3f3f3'
+    }
+  }
+}))(TableRow)
 
 const CodeListItem = ({ short_url, full_url, created }) => {
 
-
   return (
-    <TableRow>
+    <StyledTableRow>
       <TableCell>
         <Typography>{short_url}</Typography>
       </TableCell>
@@ -20,10 +33,18 @@ const CodeListItem = ({ short_url, full_url, created }) => {
       <TableCell>
         <Typography>{created}</Typography>
       </TableCell>
-      <TableCell>
-        <Typography>icons</Typography>
+      <TableCell align='center'>
+        <IconButton>
+          <InfoOutlinedIcon fontSize='large'/>
+        </IconButton>
+        <IconButton>
+          <EditOutlinedIcon color='primary' fontSize='large'/>
+        </IconButton>
+        <IconButton>
+          <DeleteOutlinedIcon color='secondary' fontSize='large'/>
+        </IconButton>
       </TableCell>
-    </TableRow>
+    </StyledTableRow>
   )
 }
 
