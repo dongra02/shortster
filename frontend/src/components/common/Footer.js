e1234185305b3d0737bc7b0c8cc8dcbf98546b8b
dongra02 <dsgraham1@gmail.com>
2021-01-20 15:31:04 +0200

    Styling - Footer link added

--- frontend/src/components/common/Footer.js
+++ frontend/src/components/common/Footer.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import Grid from '@material-ui/core/Grid'
-import { StyledFooterGrid, StyledFooterType } from '../../elements/FooterGrid'
+// import Link from '@material-ui/core/Link'
+import { StyledFooterGrid, StyledFooterType, StyledFootLink } from '../../elements/FooterGrid'
 
 const Footer = () => {
 
   return (
-    <StyledFooterGrid container spacing={2}>
+    <StyledFooterGrid container spacing={2} alignItems='center'>
       <Grid item>
-        <StyledFooterType>An Application by Don</StyledFooterType>
+        <StyledFooterType>An Application by <StyledFootLink href='https://www.don-graham.dev' target='_blank'>don-graham.dev</StyledFootLink></StyledFooterType>
       </Grid>
     </StyledFooterGrid>
   )
--- frontend/src/elements/FooterGrid.js
+++ frontend/src/elements/FooterGrid.js
@@ -1,11 +1,12 @@
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
 import { withStyles } from '@material-ui/core/styles'
 
 
 export const StyledFooterGrid = withStyles((theme) => ({
   root: {
-    position: 'fixed',
+    position: 'absolute',
     justifyContent: 'center',
     bottom: 0,
     backgroundColor: theme.palette.primary.main,
@@ -18,3 +19,11 @@ export const StyledFooterType = withStyles(() => ({
     fontSize: '.8rem'
   }
 }))(Typography)
+
+export const StyledFootLink = withStyles(() => ({
+  root: {
+    color: '#fff',
+    fontSize: '.8rem',
+    textDecoration: 'underline'
+  }
+}))(Link)
